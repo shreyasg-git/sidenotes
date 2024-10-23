5bd9d3cdb561d015efcd578d82b110281376a64d
shreyasbg <shreyasbgangurde@gmail.com>
2024-10-23 18:03:10 +0530

    copy, clear, scroll buttons, github link

--- sidepanel.js
+++ sidepanel.js
@@ -1,5 +1,41 @@
 // Get the textarea element
 const notepad = document.getElementById("notepad");
+const scrollbtn = document.getElementById("scrollbtn");
+const copybtn = document.getElementById("copybtn");
+const clearbtn = document.getElementById("clearbtn");
+
+scrollbtn.innerText = "v";
+
+copybtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(notepad.value);
+});
+
+clearbtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(notepad.value);
+  if (confirm("Clear All Text?")) {
+    notepad.value = "";
+  } else {
+    return;
+  }
+});
+
+scrollbtn.addEventListener("click", () => {
+  console.log("AAA", scrollbtn.innerText);
+  if (scrollbtn.innerText === "^") {
+    console.log("SCROLL TO TOP : 0");
+    notepad.scrollTo({ top: 0, behavior: "smooth" });
+
+    scrollbtn.innerText = "v";
+    return;
+  }
+  if (scrollbtn.innerText === "v") {
+    console.log("SCROLL TO BOTTOM : ", notepad.scrollHeight);
+
+    notepad.scrollTo({ top: notepad.scrollHeight, behavior: "smooth" });
+    scrollbtn.innerText = "^";
+    return;
+  }
+});
 
 // Load saved text when the panel opens
 chrome.storage.local.get(["noteText"], function (result) {
